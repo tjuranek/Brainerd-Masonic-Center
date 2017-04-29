349bbe84d6b0725b5ca329d007fe7a7bddd417e5
Nathanial S. Lubitz <nate.lubitz@gmail.com>
2017-04-28 20:12:00 -0500

    Saved push notifications to local storage

--- BMC/www/js/settings.js
+++ BMC/www/js/settings.js
@@ -1,8 +1,19 @@
 angular.module('bmc.settings', [])
 
-.controller('settings', function($scope) {
-
-
+  .controller('settings', function($scope) {
+    //try reading from JSON if it already exists
+    try {
+      var all = JSON.parse(window.localStorage.getItem("pushNotifications"));
+      console.log(all);
+      var notif_Aurora = all.aurora;
+      var notif_Addzuhman = all.addzuhman;
+      var notif_OES = all.oes;
+      var notif_Band = all.band;
+      var notif_Aux = all.aux;
+      var allow_notif = all.notif;
+    }
+//if it doesn't exist, create it and initialize values
+    catch (eee) {
       var notif_Aurora = false;
       var notif_Addzuhman = false;
       var notif_OES = false;
@@ -10,68 +21,81 @@ angular.module('bmc.settings', [])
       var notif_Aux = false;
       var allow_notif = true;
       $scope.NotifChecked = true;
+    }
+    $scope.NotifChecked = allow_notif;
+    $scope.AuxiliaryChecked = notif_Aux;
+    $scope.AuroraChecked = notif_Aurora;
+    $scope.OESChecked = notif_OES;
+    $scope.AddzuhmanChecked = notif_Addzuhman;
+    $scope.BandChecked = notif_Band;
+    if (allow_notif) {
+      document.getElementById('push').style.display = 'block';
+    } else {
+      document.getElementById('push').style.display = 'none';
+    }
 
-$scope.notifChanged = function(){
-  if (allow_notif){
-    allow_notif = false;
-    document.getElementById('push').style.display = 'none';
-  }
-  else {
-    allow_notif = true;
-    document.getElementById('push').style.display = 'block';
-  }
-  updateInformation();
-}
-  $scope.AuroraChanged= function() {
-      if(notif_Aurora)
-      notif_Aurora = false;
+
+    $scope.notifChanged = function() {
+      if (allow_notif) {
+        allow_notif = false;
+        document.getElementById('push').style.display = 'none';
+      } else {
+        allow_notif = true;
+        document.getElementById('push').style.display = 'block';
+      }
+      updateInformation();
+    }
+    $scope.AuroraChanged = function() {
+      if (notif_Aurora)
+        notif_Aurora = false;
       else {
         notif_Aurora = true;
       }
-    updateInformation();
-  }
+      updateInformation();
+    }
 
-      $scope.AddzuhmanChanged= function() {
-        if(notif_Addzuhman)
+    $scope.AddzuhmanChanged = function() {
+      if (notif_Addzuhman)
         notif_Addzuhman = false;
-        else {
-          notif_Addzuhman = true;
-        }
-        updateInformation();
+      else {
+        notif_Addzuhman = true;
       }
-        $scope.OESChanged= function() {
-          if(notif_OES)
-          notif_OES = false;
-          else {
-            notif_OES = true;
-          }
-          updateInformation();
-        }
-          $scope.BandChanged= function() {
-            if(notif_Band)
-            notif_Band = false;
-            else {
-              notif_Band = true;
-            }
-        updateInformation();
-          }
-            $scope.AuxiliaryChanged= function() {
-              if(notif_Aux)
-              notif_Aux = false;
-              else {
-                notif_Aux = true;
-              }
-              updateInformation();
-            }
-var updateInformation = function(){
-  console.log("All Information:");
-  console.log(allow_notif);
-  console.log(notif_Aurora);
-  console.log(notif_Addzuhman);
-  console.log(notif_OES);
-  console.log(notif_Band);
-  console.log(notif_Aux);
-}
-
-
+      updateInformation();
+    }
+    $scope.OESChanged = function() {
+      if (notif_OES)
+        notif_OES = false;
+      else {
+        notif_OES = true;
+      }
+      updateInformation();
+    }
+    $scope.BandChanged = function() {
+      if (notif_Band)
+        notif_Band = false;
+      else {
+        notif_Band = true;
+      }
+      updateInformation();
+    }
+    $scope.AuxiliaryChanged = function() {
+      if (notif_Aux)
+        notif_Aux = false;
+      else {
+        notif_Aux = true;
+      }
+      updateInformation();
+    }
+    var updateInformation = function() {
+      var appData = {
+        notif: allow_notif,
+        aurora: notif_Aurora,
+        addzuhman: notif_Addzuhman,
+        oes: notif_OES,
+        band: notif_Band,
+        aux: notif_Aux
+      };
+      window.localStorage.setItem("pushNotifications", JSON.stringify(appData));
+      console.log(window.localStorage.getItem("pushNotifications"));
+    };
   });
