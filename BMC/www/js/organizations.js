3843b09c20bb57265f6bb4ab8c6558f7b809e199
Thomas Juranek <thomas@juranek.com>
2017-04-29 23:59:19 -0500

    Organizaition Subpage Navigation

--- BMC/www/js/organizations.js
+++ BMC/www/js/organizations.js
@@ -1,14 +1,15 @@
 angular.module('bmc.organizations', [])
 
-.controller('organizations', function($scope) {
+.controller('organizations', function($state, $scope) {
+    var storage = window.localStorage;
 
-    var myName = "thomas";
-    $scope.myName = "thomasjuranek";
+    $scope.goToOrg = function(organization) {
+        storage.setItem('currentSubOrganization', organization);
+        $state.go('app.sub_organization');
+    }
 })
 
 .controller('sub_organizations', function($scope) {
     var storage = window.localStorage;
-    var value = storage.getItem(key); // Pass a key name to get its value.
-    storage.setItem(key, value) // Pass a key name and its value to add or update that key.
-    storage.removeItem(key) // Pass a key name to remove that key from storage.
+    $scope.name = storage.getItem('currentSubOrganization');
 });
