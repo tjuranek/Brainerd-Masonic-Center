728cb3ad94494589fec5e2271a1465c82d6dfa69
Thomas Juranek <thomas@juranek.com>
2017-05-08 00:37:21 -0500

    Config Stuff

--- BMC/www/js/organizations.js
+++ BMC/www/js/organizations.js
@@ -1,15 +1,12 @@
 angular.module('bmc.organizations', [])
 
 .controller('organizations', function($state, $scope) {
-    var storage = window.localStorage;
-
-    $scope.goToOrg = function(organization) {
-        storage.setItem('currentSubOrganization', organization);
-        $state.go('app.sub_organization');
-    }
+  $scope.goToOrg = function(organization) {
+    window.localStorage.setItem('currentSubOrganization', organization);
+    $state.go('app.sub_organization');
+  }
 })
 
 .controller('sub_organizations', function($scope) {
-    var storage = window.localStorage;
-    $scope.name = storage.getItem('currentSubOrganization');
+  var currentSubOrganization = window.localStorage.getItem('currentSubOrganization');
 });
